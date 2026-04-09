b9a47bd7b2e21937f5255529e8377eda7bb2ec20
ayushisharma-05 <ayushi.sharma5205@gmail.com>
2026-04-09 23:55:37 +0530

    Add files via upload

---
+++ Arrays/EvenNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class EvenNumber{
+        public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println ("Enter a number ");
+        n = sc.nextInt();
+
+    }
+}
